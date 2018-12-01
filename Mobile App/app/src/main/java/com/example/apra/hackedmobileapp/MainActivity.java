711e3c4e8f40df75d2df08e3aff953d9f408e8e1
Apra Gupta <apra@apra.in>
2018-12-01 17:54:58 -0500

    few screens

--- Mobile App/app/src/main/java/com/example/apra/hackedmobileapp/MainActivity.java
+++ Mobile App/app/src/main/java/com/example/apra/hackedmobileapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.apra.hackedmobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,8 +26,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
             }
         });
+
+       Button topic = (Button)findViewById(R.id.button3);
+
+       topic.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               startActivity(new Intent(MainActivity.this,Main2Activity.class));
+           }
+       });
+
     }
 
     @Override
